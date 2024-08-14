cb8f19c8fc1294271f9c0a95e40fe2fce1473101
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2024-08-14 11:54:46 +0200

    Add the sample code of the thread 57960885: "Using IFileProvider to watch a directory to get file changes".
    Link to the thread: https://stackoverflow.com/questions/57960885/using-ifileprovider-to-watch-a-directory-to-get-file-changes/

---
+++ 57960885/FileSystemWatcherExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FileSystemWatcherExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.WatchDirectory("C:\\Temp", "Test.txt");
+
+            Console.WriteLine("Watching changes...");
+            Console.ReadLine();
+        }
+
+        private void WatchDirectory(string directoryPath, string filter)
+        {
+            var fileWatcher = new FileSystemWatcher(directoryPath, filter);
+            fileWatcher.Changed += this.OnFileChanged;
+            fileWatcher.Created += this.OnFileChanged;
+            fileWatcher.Deleted += this.OnFileChanged;
+            fileWatcher.Renamed += this.OnFileChanged;
+
+            fileWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File updated: {e.Name} (ChangeType: {e.ChangeType})");
+        }
+    }
+}
